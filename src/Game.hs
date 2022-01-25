f9a88013bd7aee32621dec747b8ac81b3491c2a7
Harald Gliebe <harald.gliebe@gmail.com>
2022-01-25 17:38:45 +0100

    Unit and property tests for the game

--- src/Game.hs
+++ src/Game.hs
@@ -8,11 +8,25 @@ import Data.Functor ((<&>))
 import qualified Data.Set as Set
 import Domain
 
-score :: [Card] -> Int
-score = sum . map cardValue
+{--|
+  Plays a game of Twentyone with the given deck.
 
-resultWithWinner :: Participant -> GameState -> GameResult
-resultWithWinner p GameState {..} = GameFinished {winner = p, cardsSam = handSam, cardsDealer = handDealer}
+  This is the main entry point into the Game module.
+--}
+play :: [Card] -> GameResult
+play deck =
+  let initialState = GameState {handSam = [], handDealer = [], deck = deck}
+   in Right initialState
+        >>= dealCards
+        >>= checkInitialHands
+        >>= samDraws
+        >>= checkIfSamLost
+        >>= dealerDraws
+        >>= checkIfDealerLost
+        & determineWinner
+
+-- Steps in the game
+-- These functins are only exported from the module for unit tests
 
 dealCards :: GameState -> Either GameResult GameState
 dealCards GameState {deck = s1 : d1 : s2 : d2 : rest} =
@@ -25,39 +39,42 @@ checkInitialHands state@GameState {..} =
     (21, _) -> Left $ resultWithWinner Sam state
     (_, 21) -> Left $ resultWithWinner Dealer state
     (22, 22) -> Left $ resultWithWinner Dealer state
-    _ -> Right state -- no winner yet
+    _ -> Right state -- no winner yet, continue
 
 -- take cards until score is over 17
-playSam :: GameState -> Either GameResult GameState
-playSam state@GameState {handSam} | score handSam >= 17 = Right state
-playSam state@GameState {handSam, deck = c : cs} = playSam $ state {handSam = handSam ++ [c], deck = cs}
-playSam _ = Left NotEnoughCards
+samDraws :: GameState -> Either GameResult GameState
+samDraws state@GameState {handSam} | score handSam >= 17 = Right state
+samDraws state@GameState {handSam, deck = c : cs} = samDraws $ state {handSam = handSam ++ [c], deck = cs}
+samDraws _ = Left NotEnoughCards
 
-didSamLose :: GameState -> Either GameResult GameState
-didSamLose state =
-  if score (handSam state) > 21 then Left $ resultWithWinner Dealer state else Right state
+-- Sam loses if his hand scores more than 21
+checkIfSamLost :: GameState -> Either GameResult GameState
+checkIfSamLost state | score (handSam state) > 21 = Left $ resultWithWinner Dealer state
+checkIfSamLost state = Right state
 
--- take cards until score is over 17
-playDealer :: GameState -> Either GameResult GameState
-playDealer state@GameState {..} | score handDealer > score handSam = Right state
-playDealer state@GameState {handDealer, deck = c : cs} = playDealer $ state {handDealer = handDealer ++ [c], deck = cs}
-playDealer _ = Left NotEnoughCards
+-- take cards until score is higher than Sam's
+dealerDraws :: GameState -> Either GameResult GameState
+dealerDraws state@GameState {..} | score handDealer > score handSam = Right state
+dealerDraws state@GameState {handDealer, deck = c : cs} = dealerDraws $ state {handDealer = handDealer ++ [c], deck = cs}
+dealerDraws _ = Left NotEnoughCards
 
-finalResult :: Either GameResult GameState -> GameResult
-finalResult (Left result) = result
-finalResult (Right state@GameState {..}) =
+-- The dealer loses if her hand scores more than 21
+checkIfDealerLost :: GameState -> Either GameResult GameState
+checkIfDealerLost state | score (handDealer state) > 21 = Left $ resultWithWinner Sam state
+checkIfDealerLost state = Right state
+
+determineWinner :: Either GameResult GameState -> GameResult
+determineWinner (Left result) = result
+determineWinner (Right state@GameState {..}) =
   let scoreSam = score handSam
       scoreDealer = score handDealer
-      winner = if scoreDealer <= 21 && scoreDealer > scoreSam || scoreSam > 21 then Dealer else Sam
+      winner = if scoreDealer > scoreSam then Dealer else Sam
    in resultWithWinner winner state
 
-play :: [Card] -> GameResult
-play deck =
-  let initialState = GameState {handSam = [], handDealer = [], deck = deck}
-   in Right initialState
-        >>= dealCards
-        >>= checkInitialHands
-        >>= playSam
-        >>= didSamLose
-        >>= playDealer
-        & finalResult
+-- Helper functions
+
+score :: [Card] -> Int
+score = sum . map cardValue
+
+resultWithWinner :: Participant -> GameState -> GameResult
+resultWithWinner p GameState {..} = GameFinished {winner = p, cardsSam = handSam, cardsDealer = handDealer}
--- test/Test.hs
+++ test/Test.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
 
+import Data.List (isPrefixOf)
 import Domain
-import Game (play)
+import Game
 import Parser
 import Test.Hspec
+import Test.QuickCheck
 
 parserSpec :: Spec
 parserSpec = describe "parser" $ do
@@ -25,54 +29,271 @@ displaySpec = describe "displayGameResult" $ do
 
 gameRuleSpec :: Spec
 gameRuleSpec = do
+  describe "Game" $ do
+    describe "dealCards" $ do
+      it "deal the cards in the order [sam, dealer, sam, dealer]" $
+        do
+          dealCards
+            GameState
+              { handSam = [],
+                handDealer = [],
+                deck = [Card Clubs Jack, Card Hearts Queen, Card Clubs King, Card Hearts Ace]
+              }
+          `shouldBe` Right
+            GameState
+              { handSam = [Card Clubs Jack, Card Clubs King],
+                handDealer = [Card Hearts Queen, Card Hearts Ace],
+                deck = []
+              }
+    describe "checkInitialHands" $ do
+      it "sam wins if he has Blackjack in his initial hand" $
+        do
+          checkInitialHands
+            GameState
+              { handSam = [Card Clubs Jack, Card Clubs Ace],
+                handDealer = [Card Hearts Queen, Card Hearts Two],
+                deck = []
+              }
+          `shouldBe` Left
+            GameFinished
+              { winner = Sam,
+                cardsSam = [Card Clubs Jack, Card Clubs Ace],
+                cardsDealer = [Card Hearts Queen, Card Hearts Two]
+              }
+      it "dealer wins if she has Blackjack in her initial hand" $
+        do
+          checkInitialHands
+            GameState
+              { handSam = [Card Hearts Jack, Card Diamonds Four],
+                handDealer = [Card Clubs Jack, Card Diamonds Ace],
+                deck = []
+              }
+          `shouldBe` Left
+            GameFinished
+              { winner = Dealer,
+                cardsSam = [Card Hearts Jack, Card Diamonds Four],
+                cardsDealer = [Card Clubs Jack, Card Diamonds Ace]
+              }
+      it "sam wins if both have Blackjack in their initial hand" $
+        do
+          checkInitialHands
+            GameState
+              { handSam = [Card Hearts Ace, Card Clubs Jack],
+                handDealer = [Card Spades Jack, Card Diamonds Ace],
+                deck = []
+              }
+          `shouldBe` Left
+            GameFinished
+              { winner = Sam,
+                cardsSam = [Card Hearts Ace, Card Clubs Jack],
+                cardsDealer = [Card Spades Jack, Card Diamonds Ace]
+              }
+      it "dealer wins if both have a score of 22 in their initial hand" $
+        do
+          checkInitialHands
+            GameState
+              { handSam = [Card Hearts Ace, Card Clubs Ace],
+                handDealer = [Card Spades Ace, Card Diamonds Ace],
+                deck = []
+              }
+          `shouldBe` Left
+            GameFinished
+              { winner = Dealer,
+                cardsSam = [Card Hearts Ace, Card Clubs Ace],
+                cardsDealer = [Card Spades Ace, Card Diamonds Ace]
+              }
+    describe "samDraws" $ do
+      it "if neither player has Blackjack then sam can start drawing cards from the top of the deck" $
+        do
+          samDraws
+            GameState
+              { handSam = [Card Hearts Ace, Card Clubs Five],
+                handDealer = [Card Diamonds Ten, Card Spades Ten],
+                deck = [Card Spades Seven]
+              }
+          `shouldBe` Right
+            GameState
+              { handSam = [Card Hearts Ace, Card Clubs Five, Card Spades Seven],
+                handDealer = [Card Diamonds Ten, Card Spades Ten],
+                deck = []
+              }
+      it "Sam can draw multiple Cards as long as his score is below 17" $
+        do
+          samDraws
+            GameState
+              { handSam = [Card Hearts Three, Card Clubs Five],
+                handDealer = [Card Diamonds Ten, Card Spades Ten],
+                deck = [Card Hearts Two, Card Clubs Two, Card Spades Two, Card Diamonds Seven]
+              }
+          `shouldBe` Right
+            GameState
+              { handSam = [Card Hearts Three, Card Clubs Five, Card Hearts Two, Card Clubs Two, Card Spades Two, Card Diamonds Seven],
+                handDealer = [Card Diamonds Ten, Card Spades Ten],
+                deck = []
+              }
+      it "sam must stop drawing cards from the deck if their total reaches 17 or higher" $
+        do
+          samDraws
+            GameState
+              { handSam = [Card Hearts Ace, Card Diamonds Seven],
+                handDealer = [Card Clubs Ten, Card Spades Ten],
+                deck = [Card Spades Five]
+              }
+          `shouldBe` Right
+            GameState
+              { handSam = [Card Hearts Ace, Card Diamonds Seven],
+                handDealer = [Card Clubs Ten, Card Spades Ten],
+                deck = [Card Spades Five]
+              }
+    describe "checkIfSamLost" $
+      it "sam has lost the game if their total is higher than 21" $
+        do
+          checkIfSamLost
+            GameState
+              { handSam = [Card Hearts Ace, Card Clubs Five, Card Spades Seven],
+                handDealer = [Card Diamonds Ten, Card Spades Ten],
+                deck = [Card Spades Five]
+              }
+          `shouldBe` Left
+            GameFinished
+              { winner = Dealer,
+                cardsSam = [Card Hearts Ace, Card Clubs Five, Card Spades Seven],
+                cardsDealer = [Card Diamonds Ten, Card Spades Ten]
+              }
+    describe "dealerDraws" $ do
+      it "when sam has stopped drawing cards the dealer can start drawing cards from the top of the deck" $
+        do
+          dealerDraws
+            GameState
+              { handSam = [Card Hearts Ace, Card Diamonds Seven],
+                handDealer = [Card Clubs Ten, Card Spades Four],
+                deck = [Card Spades Five]
+              }
+          `shouldBe` Right
+            GameState
+              { handSam = [Card Hearts Ace, Card Diamonds Seven],
+                handDealer = [Card Clubs Ten, Card Spades Four, Card Spades Five],
+                deck = []
+              }
+      it "the dealer must stop drawing cards when their total is higher than sam." $
+        do
+          dealerDraws
+            GameState
+              { handSam = [Card Hearts Ace, Card Diamonds Seven],
+                handDealer = [Card Clubs Ten, Card Spades Ten],
+                deck = [Card Spades Five]
+              }
+          `shouldBe` Right
+            GameState
+              { handSam = [Card Hearts Ace, Card Diamonds Seven],
+                handDealer = [Card Clubs Ten, Card Spades Ten],
+                deck = [Card Spades Five]
+              }
+    describe "checkIfDealerLost" $
+      it "the dealer has lost the game if their total is higher than 21" $
+        do
+          checkIfDealerLost
+            GameState
+              { handSam = [Card Diamonds Ten, Card Spades Ten],
+                handDealer = [Card Hearts Ace, Card Clubs Five, Card Spades Seven],
+                deck = []
+              }
+          `shouldBe` Left
+            GameFinished
+              { winner = Sam,
+                cardsSam = [Card Diamonds Ten, Card Spades Ten],
+                cardsDealer = [Card Hearts Ace, Card Clubs Five, Card Spades Seven]
+              }
+    describe "determineWinner" $
+      it "determines which player wins the game (highest score wins)" $
+        do
+          determineWinner
+            ( Right
+                GameState
+                  { handSam = [Card Diamonds Ten, Card Spades Ten],
+                    handDealer = [Card Hearts Ace, Card Clubs Five, Card Spades Five],
+                    deck = []
+                  }
+            )
+          `shouldBe` GameFinished
+            { winner = Dealer,
+              cardsSam = [Card Diamonds Ten, Card Spades Ten],
+              cardsDealer = [Card Hearts Ace, Card Clubs Five, Card Spades Five]
+            }
+    it "Example from specification" $
+      do
+        let result = play [Card Clubs Ace, Card Diamonds Five, Card Hearts Nine, Card Hearts Queen, Card Spades Eight]
+        result
+          `shouldBe` GameFinished
+            { winner = Sam,
+              cardsSam = [Card Clubs Ace, Card Hearts Nine],
+              cardsDealer = [Card Diamonds Five, Card Hearts Queen, Card Spades Eight]
+            }
+    it "Edge case: not enough cards for dealing" $
+      do
+        let result = play [Card Hearts Ace, Card Diamonds Two, Card Clubs Seven]
+        result `shouldBe` NotEnoughCards
+    it "Edge case: not enough cards for Sam" $
+      do
+        let result = play [Card Hearts Ace, Card Diamonds Seven, Card Clubs Two, Card Spades Nine]
+        result `shouldBe` NotEnoughCards
+    it "Edge case: not enough cards for dealer" $
+      do
+        let result = play [Card Hearts Ace, Card Diamonds Two, Card Clubs Seven, Card Spades Nine]
+        result `shouldBe` NotEnoughCards
+    it "Edge case: Sam can loose although dealer has an initial hand of score 22" $
+      do
+        let result = play [Card Hearts Ace, Card Diamonds Ace, Card Clubs Four, Card Spades Ace, Card Spades Nine]
+        winner result `shouldBe` Dealer
+
+newtype Deck = Deck [Card] deriving (Show)
+
+-- consider only complete decks, so we alwasy have a winner
+-- the edge cases with too few cards are already handled in the unit tests
+instance Arbitrary Deck where
+  arbitrary = Deck <$> shuffle fullDeck
+
+-- Cards are dealt in the correct order
+prop_CardOrder :: Deck -> Bool
+prop_CardOrder (Deck cards) =
+  let GameFinished {cardsDealer = d1 : d2 : dRest, cardsSam = s1 : s2 : sRest} = play cards
+   in (s1 : d1 : s2 : d2 : sRest ++ dRest) `isPrefixOf` cards
+
+-- before Sam's last draw, his hand should have a score below 17
+-- this is trivially true, if Sam didn't take any card
+prop_SamStopsAt17 :: Deck -> Bool
+prop_SamStopsAt17 (Deck cards) =
+  let result = play cards
+   in score (init $ cardsSam result) < 17
+
+-- If Sam's score is greater than 21 he loses
+-- this is also the case in the initial phase, even when the dealer has a score of 22
+prop_SamLosesWithScoreHigherThan21 :: Deck -> Bool
+prop_SamLosesWithScoreHigherThan21 (Deck cards) =
+  let GameFinished {..} = play cards
+   in not (score cardsSam > 21 && winner == Sam)
+
+-- If the dealer took an additional card, her score must have been not more than Sam's
+prop_DealerStopsHigherThanSam :: Deck -> Bool
+prop_DealerStopsHigherThanSam (Deck cards) =
+  let GameFinished {..} = play cards
+   in length cardsDealer == 2 || score (init cardsDealer) <= score cardsSam
+
+gamePropertySpec :: Spec
+gamePropertySpec =
   describe "play" $ do
-    it "sam wins if he has Blackjack in his initial hand" $
-      do
-        play [Card Hearts Ace, Card Spades Two, Card Clubs Jack, Card Diamonds Four]
-        `shouldBe` GameFinished Sam [Card Hearts Ace, Card Clubs Jack] [Card Spades Two, Card Diamonds Four]
-    it "dealer wins if she has Blackjack in her initial hand" $
-      do
-        play [Card Hearts Ace, Card Clubs Jack, Card Diamonds Four, Card Diamonds Ace]
-        `shouldBe` GameFinished Dealer [Card Hearts Ace, Card Diamonds Four] [Card Clubs Jack, Card Diamonds Ace]
-    it "sam wins if both have Blackjack in their initial hand" $
-      do
-        play [Card Hearts Ace, Card Spades Jack, Card Clubs Jack, Card Diamonds Ace]
-        `shouldBe` GameFinished Sam [Card Hearts Ace, Card Clubs Jack] [Card Spades Jack, Card Diamonds Ace]
-    it "dealer wins if both have a score of 22 in their initial hand" $
-      do
-        play [Card Hearts Ace, Card Clubs Ace, Card Diamonds Ace, Card Spades Ace]
-        `shouldBe` GameFinished Dealer [Card Hearts Ace, Card Diamonds Ace] [Card Clubs Ace, Card Spades Ace]
-    it "if neither player has Blackjack then sam can start drawing cards from the top of the deck" $
-      do
-        let result = play [Card Hearts Ace, Card Diamonds Ten, Card Clubs Five, Card Spades Ten, Card Spades Seven]
-        cardsSam result `shouldBe` [Card Hearts Ace, Card Clubs Five, Card Spades Seven]
+    it "cards are dealt in the correct order" $
+      property prop_CardOrder
     it "sam must stop drawing cards from the deck if their total reaches 17 or higher" $
-      do
-        let result = play [Card Hearts Ace, Card Clubs Ten, Card Diamonds Seven, Card Spades Ten, Card Spades Five]
-        cardsSam result `shouldBe` [Card Hearts Ace, Card Diamonds Seven]
+      property prop_SamStopsAt17
     it "sam has lost the game if their total is higher than 21" $
-      do
-        let result = play [Card Hearts Ace, Card Diamonds Ten, Card Clubs Five, Card Spades Ten, Card Spades Seven]
-        winner result `shouldBe` Dealer
-    it "when sam has stopped drawing cards the dealer can start drawing cards from the top of the deck" $
-      do
-        let result = play [Card Hearts Ace, Card Diamonds Ten, Card Clubs Seven, Card Spades Six, Card Spades Seven]
-        cardsDealer result `shouldBe` [Card Diamonds Ten, Card Spades Six, Card Spades Seven]
-    it "the dealer must stop drawing cards when their total is higher than sam." $
-      do
-        let result = play [Card Hearts Ace, Card Diamonds Ten, Card Clubs Seven, Card Spades Nine, Card Spades Seven]
-        cardsDealer result `shouldBe` [Card Diamonds Ten, Card Spades Nine]
-    it "the dealer has lost the game if their total is higher than 21" $
-      do
-        let result = play [Card Hearts Ace, Card Diamonds Ten, Card Clubs Seven, Card Spades Six, Card Spades Seven]
-        winner result `shouldBe` Sam
-    it "determine which player wins the game (highest score wins)" $
-      do
-        let result = play [Card Hearts Ace, Card Diamonds Ten, Card Clubs Seven, Card Spades Nine]
-        winner result `shouldBe` Dealer
+      property prop_SamLosesWithScoreHigherThan21
+    it "the dealer must stop drawing cards when their total is higher than sam" $
+      property prop_DealerStopsHigherThanSam
 
 main :: IO ()
 main = hspec $ do
   displaySpec
   parserSpec
   gameRuleSpec
+  gamePropertySpec
